53e0e4c6d2c227700beb8eb2cc638477383145c1
visantan <vitoria.757565@graduacao.fsa.br>
2025-10-24 19:54:35 -0300

    site to run on localhost

--- src/pages/Historico.jsx
+++ src/pages/Historico.jsx
@@ -9,7 +9,7 @@ function Historico() {
   useEffect(() => {
     const fetchHistorico = async () => {
       try {
-        const response = await fetch('https://api-data-aquasense.vercel.app/api/historico');
+        const response = await fetch('http://localhost:5000/api/historico');
         const data = await response.json();
         setHistorico(data);
       } catch (error) {
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch('https://api-data-aquasense.vercel.app/api/login', {
+      const response = await fetch('http://localhost:5000/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ usuario, senha }),
