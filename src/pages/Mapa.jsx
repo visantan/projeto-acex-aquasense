33bc1b69a2d0b8bb91abb4c0097c689c28b8deda
visantan <vitoria.757565@graduacao.fsa.br>
2025-11-06 23:02:05 -0300

    Mark maps

--- src/pages/Mapa.jsx
+++ src/pages/Mapa.jsx
@@ -1,15 +1,27 @@
-// src/pages/Mapa.jsx
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 import '../css/Mapa.css';
 import ruas from '../data/ruas';
+
+import L from 'leaflet';
+
+const markerIcon = new URL('leaflet/dist/images/marker-icon.png', import.meta.url).href;
+const markerShadow = new URL('leaflet/dist/images/marker-shadow.png', import.meta.url).href;
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 function Mapa() {
   return (
     <div className="mapa-container">
       <h2>Mapa de Vazamentos e Obras</h2>
       <MapContainer
-        center={[-23.6639, -46.5383]} // Santo André
+        center={[-23.6639, -46.5383]}
         zoom={13}
         scrollWheelZoom={false}
         className="mapa"
@@ -24,8 +36,8 @@ function Mapa() {
             <Popup>
               <strong>{rua.nome}</strong><br />
               Bairro: {rua.bairro}<br />
-              {rua.status.vazamento && (<>Vazamento detectado<br/></>)}
-              {rua.status.obra && (<>Obra em andamento<br/></>)}
+              {rua.status.vazamento && (<>Vazamento detectado<br /></>)}
+              {rua.status.obra && (<>Obra em andamento<br /></>)}
               Qualidade: {rua.status.qualidade}
             </Popup>
           </Marker>
